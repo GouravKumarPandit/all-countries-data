5516917d6645c6c5c851ef82533b0aefa0e678ef
Gourav Kumar Pandit <117534016+GouravKumarPandit@users.noreply.github.com>
2024-09-22 13:38:53 +0530

    Showing all countries data, using fetch

---
+++ Javascript/script.js
@@ -0,0 +1,57 @@
+const countriesContainer = document.querySelector('.countries-container')
+const filterByRegion = document.querySelector('.filter-by-region')
+const searchInput = document.querySelector('.search-container input')
+const themeChanger = document.querySelector('.theme-changer')
+
+let allCountriesData
+
+fetch('https://restcountries.com/v3.1/all')
+	.then((res) => res.json())
+	.then((data) => {
+		renderCountries(data)
+		allCountriesData = data
+	})
+
+filterByRegion.addEventListener('change', (e) => {
+	fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+		.then((res) => res.json())
+		.then(renderCountries)
+})
+
+function renderCountries(data) {
+	countriesContainer.innerHTML = ''
+	data.forEach((country) => {
+		const countryCard = document.createElement('a')
+		countryCard.classList.add('country-card')
+		countryCard.href = `/country.html?name=${country.name.common}`
+		countryCard.innerHTML = `
+          <img src="${country.flags.svg}" alt="${country.name.common} Flag" />
+          <div class="card-text">
+              <h3 class="card-title">${country.name.common}</h3>
+              <p><b>Population: </b>${country.population.toLocaleString(
+			'en-IN'
+		)}</p>
+              <p><b>Region: </b>${country.region}</p>
+              <p><b>Capital: </b>${country.capital?.[0]}</p>
+          </div>
+  `
+		countriesContainer.append(countryCard)
+	})
+}
+
+
+searchInput.addEventListener('input', (e) => {
+	const filteredCountries = allCountriesData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+	renderCountries(filteredCountries)
+})
+
+themeChanger.addEventListener('click', () => {
+	document.body.classList.toggle('dark')
+	const lightDarkMode = document.querySelector('#light-dark-mode')
+	if(lightDarkMode.innerText === "Light Mode"){
+		themeChanger.innerHTML = '<i class="fa-regular fa-moon"></i>&nbsp;&nbsp;<span id="light-dark-mode">Dark Mode</span>' 
+	} else{
+		themeChanger.innerHTML = '<i class="fa-solid fa-sun"></i>&nbsp;&nbsp;<span id="light-dark-mode">Light Mode</span>'
+	}
+})
+
